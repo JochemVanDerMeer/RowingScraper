e5d47563fe7c6a7e0b41ae54d5d8fff62cda120a
jmeer <j.vandermeer@student.science.ru.nl>
2022-02-08 12:40:07 +0100

    add title field finder

--- scrape.py
+++ scrape.py
@@ -4,27 +4,24 @@ import requests
 from io import StringIO
 from html.parser import HTMLParser
 
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.text = StringIO()
-    def handle_data(self, d):
-        self.text.write(d)
-    def get_data(self):
-        return self.text.getvalue()
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
+#relevant fields for Phocas
+fields = ["HE", "DE", "LHE", "LDE", "HSB", "DSB", "LHSB", "LDSB", "HG", "DG", "LHG", "LDG", "HEj8", "DEj8", "LHEj8", "LDEj4", "HEj4", "DEj4", "LHEj4", "HCl8", "DCl8", "HEj 8", "DEj 8", "LHEj 8", "LDEj 4", "HEj 4", "DEj 4", "LHEj 4", "HCl 8", "DCl 8" ]
 
 url = "https://regatta.time-team.nl/nkir/2021/results/r887e9218-413b-458d-b96b-11443b7617fb.php#19ea174d-de19-460f-9537-8ffcbf75a566"
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
+
+title = "Geen titel gevonden"
+title = doc.find_all("h2")
+title = title[0].get_text()
+print(title)
+matches = []
+for i in fields:
+    if i in title:
+        matches.append(i)
+print(max(matches, key=len))
+
 
 results = []
 for even in doc.find_all("tr", {"class": "even"}):

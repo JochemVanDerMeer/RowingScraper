68c029e87f858a47c48b51770d5b71ee53bd8230
Jochem van der Meer <jochemnov@icloud.com>
2022-02-28 20:32:37 +0100

    add excel

--- createSheet.py
+++ createSheet.py
@@ -8,6 +8,7 @@ wb = Workbook()
 sheet1 = wb.add_sheet('Sheet 1')
 
 sheet1.write(1, 0, 'ISBT DEHRADUN')
+sheet1.write(1, 1, 'tasItasesfsSBT DEHRADUN')
 sheet1.write(2, 0, 'SHASTRADHARA')
 sheet1.write(3, 0, 'CLEMEN TOWN')
 sheet1.write(4, 0, 'RAJPUR ROAD')
